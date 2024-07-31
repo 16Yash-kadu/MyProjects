0c22b6ef118602a6f073dd1012d1fb39546986c0
yash kadu <yash.k>
2024-08-01 00:41:23 +0530

    first commit

---
+++ src/utils/common.js
@@ -0,0 +1,21 @@
+export const sliderSettings = {
+  slidesPerView: 1,
+  spaceBetween: 50,
+  breakPoints: {
+    480: {
+      slidesPerView: 1,
+    },
+    600: {
+      slidesPerView: 2,
+    },
+    750: {
+      slidesPerView: 3,
+    },
+    1100: {
+      slidesPerView: 4,
+    },
+    1250: {
+      slidesPerView: 6,
+    },
+  },
+};
